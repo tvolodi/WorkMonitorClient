9f426be85e7274baa11ea701f4608b0c21e94970
tvolodi <tvolodi@gmail.com>
2023-10-20 16:08:02 +0600

    Added tray

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,20 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import java.awt.DisplayMode
-import java.awt.GraphicsEnvironment
-import java.awt.Rectangle
-import java.awt.Robot
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.AccountCircle
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.window.*
+import java.awt.*
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+
+var mainWindow: FrameWindowScope? = null
 
 @Composable
 @Preview
@@ -47,7 +46,57 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    val isOpen = remember { mutableStateOf(true) }
+    var windowScope: FrameWindowScope
+    val url = "Flat-Icons.com-Flat-Clock.16.png"
+
+//    val image: Image = BufferedImage()// Toolkit.getDefaultToolkit().getImage(url)
+
+    if(isOpen.value) {
+        Window(onCloseRequest = {
+            isOpen.value = false
+        }) { //  ::exitApplication
+            mainWindow = this
+
+            App()
+        }
     }
+
+//    createTray(this, isOpen)
+
+}
+
+fun createTray(app: ApplicationScope, isMainWindowOpen: MutableState<Boolean>): Unit {
+
+    var tray = SystemTray.getSystemTray()
+    val popup: PopupMenu =  PopupMenu()
+    // val url = System::class.java.getResource("/images/new.png")
+    val url = "Flat-Icons.com-Flat-Clock.16.png"
+    val image: Image = Toolkit.getDefaultToolkit().getImage(url)
+    val trayIcon = TrayIcon(image)
+    val aboutItem = MenuItem("About")
+
+    val displayItem = MenuItem("Display")
+    displayItem.addActionListener {
+        isMainWindowOpen.value = true
+    }
+
+    val takeScreenItem = MenuItem("Take Screenshot")
+    val exitItem = MenuItem("Exit")
+    exitItem.addActionListener {
+        app.exitApplication()
+    }
+
+    popup.add(aboutItem)
+    popup.add(displayItem)
+    popup.add(takeScreenItem)
+    popup.add(exitItem)
+    trayIcon.setPopupMenu(popup)
+
+    try {
+        tray.add(trayIcon)
+    } catch (e: AWTException) {
+        println("TrayIcon could not be added.")
+    }
+
 }
